5d33fafab203084d02d75c62d746a192c1171a07
Julius Marabe <116186260+pafeedss@users.noreply.github.com>
2023-01-02 00:08:36 +0800

    Create SeniorCustomer.java

---
+++ SeniorCustomer.java
@@ -0,0 +1,10 @@
+public class SeniorCustomer extends Customer {	
+public static final double DISCOUNT = 0.20; 	
+public SeniorCustomer(String name, double amount) {
+		super(name, amount);
+	}
+ 	@Override
+	public double calculateBill() {
+		return (amount-(amount * DISCOUNT));
+	}
+}
